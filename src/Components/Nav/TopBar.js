1aa4b47a5803a9c039fffc58438d2d95c92724a3
7p3m1k <kor0885@naver.com>
2020-07-21 13:11:31 +0900

    add:Nav바 초기UI구현

--- src/Components/Nav/TopBar.js
+++ src/Components/Nav/TopBar.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
+import "./TopBar.scss";
 
 class TopBar extends Component {
   render() {
-    return <></>;
+    return (
+      <>
+        <nav>
+          <img
+            className="nav_logo"
+            onClick=""
+            src="https://www.larocheposay.co.kr/images/common/logo.png?1"
+          ></img>
+          <ul className="top_nav">
+            <li>
+              <a href="">로그인</a>
+            </li>
+            <li>
+              <a href="">회원가입</a>
+            </li>
+            <li>
+              <a href="">주문/배송조회</a>
+            </li>
+            <li>
+              <a href="" className="top_nav_end">
+                고객케어센터
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
   }
 }
 
