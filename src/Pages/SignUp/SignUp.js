385aa3216e954b46752eeca3d30ed8ce5f1a86d3
Taehee <taehee@song-eun-us-MacBook-Pro-2.local>
2020-07-30 03:30:47 +0900

    [라로슈포제 최태희] Add: input 태그를 다루는 함수작성

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -6,7 +6,9 @@ import "./SignUp.scss";
 class SignUp extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      skinTrouble: [],
+    };
   }
 
   inputHandler = (e) => {
@@ -14,6 +16,27 @@ class SignUp extends Component {
       [e.target.name]: e.target.value,
     });
   };
+  
+  clickedSensitivity = (e) => {
+    this.setState({
+      sensitivity: Number(e.target.dataset.value)
+    })
+  }
+
+  handleCheckbox = (e) => {
+    if (e.target.checked === true) {
+      if (this.state.skinTrouble.length >= 3) {
+        alert('최대 3개 선태 가능')
+        e.target.checked = false;
+        return;
+      }
+      this.state.skinTrouble.push(e.target.value)
+      this.setState({skinTrouble: this.state.skinTrouble})
+    } else {
+      const newskinTrouble = this.state.skinTrouble.filter(el => el !== e.target.value);
+      this.setState({skinTrouble: newskinTrouble})
+    }
+  }
 
   handleSignUp = (e) => {
     fetch("http://10.58.1.97:8000/user/signup", {
@@ -25,66 +48,67 @@ class SignUp extends Component {
         phoneNumber: this.state.phoneNumber,
         name: this.state.name,
         birthday: this.state.birthday,
-        gender_type: "여자",
-        skinType: "지성",
-        skinTrouble: "건조한 피부",
+        gender_type: this.state.gender_type,
+        skinType: this.state.skinType,
+        skinTrouble: this.state.skinTrouble,
         skinSensitivity: this.state.skinSensitivity,
       })
     });
   };
 
-  render() {
-    return (
-      <div className="SignUp">
-        <div className="content">
-          <header>
-            <img
-              className="logo"
-              src="https://www.larocheposay.co.kr/images/common/logo.png?1"
-              alt="logo_image"
-            />
-          </header>
-          <main className="join_content">
-            <div className="step">
-              <div className="step_imgs">
-                <div className="step1_img"></div>
-                <div className="step_after"></div>
-                <div className="step2_img"></div>
-                <div className="step_after"></div>
-                <div className="step3_img"></div>
+    render() {
+      console.log(this.state);
+      return (
+        <div className="SignUp">
+          <div className="content">
+            <header>
+              <img
+                className="logo"
+                src="https://www.larocheposay.co.kr/images/common/logo.png?1"
+                alt="logo_image"
+              />
+            </header>
+            <main className="join_content">
+              <div className="step">
+                <div className="step_imgs">
+                  <div className="step1_img"></div>
+                  <div className="step_after"></div>
+                  <div className="step2_img"></div>
+                  <div className="step_after"></div>
+                  <div className="step3_img"></div>
+                </div>
+                <div className="step_text">
+                  <span className="step1_text">이용약관 및 본인인증</span>
+                  <span className="step2_text">정보입력</span>
+                  <span className="step3_text">가입완료</span>
+                </div>
               </div>
-              <div className="step_text">
-                <span className="step1_text">이용약관 및 본인인증</span>
-                <span className="step2_text">정보입력</span>
-                <span className="step3_text">가입완료</span>
+              <div className="join_form">
+                <h2>정보입력</h2>
+                <SignUpInfo inputHandler={this.inputHandler} />
+                <SignUpOptionalInfo clickedSensitivity={this.clickedSensitivity} handleCheckbox={this.handleCheckbox} inputHandler={this.inputHandler} skinTrouble={this.state.skinTrouble} sensitivity={this.state.sensitivity} />
               </div>
-            </div>
-            <div className="join_form">
-              <h2>정보입력</h2>
-              <SignUpInfo inputHandler={this.inputHandler} />
-              <SignUpOptionalInfo inputHandler={this.inputHandler} />
-            </div>
-            <div className="btn_wrap">
-              <button type="button">이전단계</button>
-              <button
-                onClick={this.handleSignUp}
-                className="next_btn"
-                type="button"
-              >
-                가입하기
-              </button>
-            </div>
-          </main>
-          <footer>
-            <div>
-              <span className="policy">이용약관</span>
-              <span className="privacy">개인정보처리방침</span>
-            </div>
-            <div>COPYRIGHT © 2018 LA ROCHE-POSAY ALL RIGHTS RESERVED</div>
-          </footer>
+              <div className="btn_wrap">
+                <button type="button">이전단계</button>
+                <button
+                  onClick={this.handleSignUp}
+                  className="next_btn"
+                  type="button"
+                >
+                  가입하기
+                </button>
+              </div>
+            </main>
+            <footer>
+              <div>
+                <span className="policy">이용약관</span>
+                <span className="privacy">개인정보처리방침</span>
+              </div>
+              <div>COPYRIGHT © 2018 LA ROCHE-POSAY ALL RIGHTS RESERVED</div>
+            </footer>
+          </div>
         </div>
-      </div>
-    );
+      );
   }
 }
 export default SignUp;
